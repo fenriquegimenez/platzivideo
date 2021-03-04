c95b30ce4ac357b2f472cef05199e9fc13c1cc2f
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-03-04 14:17:21 -0300

    webpack conf added

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import helloWorld from "./src/HelloWorld.jsx";
+
+ReactDOM.render(<helloWorld />, document.getElementById("app"));
---
+++ src/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom";
+
+const helloWorld = () => {
+  <h1>Hello World from React!</h1>;
+};
+
+export default helloWorld;
